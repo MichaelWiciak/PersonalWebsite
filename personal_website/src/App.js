f77f745df090ca1d897baa1e89e385897113582e
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-10-10 19:51:43 +0100

    Updated skills

--- personal_website/src/App.js
+++ personal_website/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component }  from 'react';
 import Navbar from './components/navbar/navbar';
 import Intro from './components/Intro/intro';
+import Skills from './components/Skills/skills';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Skills />
+
     </div>
   );  
 }
--- personal_website/src/components/Skills/skills.js
+++ personal_website/src/components/Skills/skills.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import './skills.css'
+import UIDesign from '../../assets/ui-design.png'
+import WebDesign from '../../assets/website-design.png'
+import AppDesign from '../../assets/app-design.png'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section id="skills">
+        <span className="skillsTitle">What I do</span>
+        <span className="skillDesc">LOREM impus lorem ipsum. Loren Inpus lorem lorem lorem impus.LOREM impus lorem ipsum. Loren Inpus lorem lorem lorem impus.LOREM impus lorem ipsum. Loren Inpus lorem lorem lorem impus.</span>
+        <div className="skillsBars">
+            <div className="skillBar">
+                <img src={UIDesign} alt="UI Design" className="skillBarImg" />
+                <div className="skillBarText">
+                    <h2>UI/UX Design</h2>
+                    <p>LOREM impus lorem ipsum. Loren Inpus lorem lorem lorem impus.</p>
+                </div>
+            </div>
+            <div className="skillBar">
+                <img src={WebDesign} alt="Web Design" className="skillBarImg" />
+                <div className="skillBarText"> 
+                    <h2>Web Design</h2>
+                    <p>LOREM impus lorem ipsum. Loren Inpus lorem lorem lorem impus.</p>
+                </div>
+            </div>
+            <div className="skillBar">
+                <img src={AppDesign} alt="App Design" className="skillBarImg" />
+                <div className="skillBarText"> 
+                    <h2>App Design</h2>
+                    <p>LOREM impus lorem ipsum. Loren Inpus lorem lorem lorem impus.</p>
+                </div>
+            </div>
+        </div>    
+    </section>
   )
 }
 
