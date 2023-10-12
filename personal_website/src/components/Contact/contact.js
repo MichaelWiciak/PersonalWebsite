0db47d9910fa08fa90943366c0be0d09ba9494d5
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-10-12 12:26:14 +0100

    N

--- personal_website/src/components/Contact/contact.js
+++ personal_website/src/components/Contact/contact.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React from "react";
+import "./contact.css";
+import FacebookIcon from "../../assets/facebook-icon.png";
+import TwitterIcon from "../../assets/twitter.png";
+import YoutubeIcon from "../../assets/youtube.png";
+import InstagramIcon from "../../assets/instagram.png";
+import Walmart from "../../assets/walmart.png";
+import Microsoft from "../../assets/microsoft.png";
+import Adobe from "../../assets/adobe.png";
+import Facebook from "../../assets/facebook.png";
 
 export const Contact = () => {
   return (
     <section id="contactPage">
-        <div id="contact">
-            <h1 className="contactPageTitle">Contact Me</h1>
-            <span className="contactDesc">Please fill the form below to contact me</span>
-            <form className="contactForm">
-                <input type="text" placeholder="Your Name..." className="name" />
-                <input type="email" placeholder="Your Email..." className="email" /> 
-                <textarea placeholder="Your Message..." className="message" rows="5"></textarea> 
-                <button type="submit" value="Send" className="submitBtn">Submit</button>
-            </form>
-
-        </div>
+      <div id="contact">
+        <h1 className="contactPageTitle">Contact Me</h1>
+        <span className="contactDesc">
+          Please fill the form below to contact me
+        </span>
+        <form className="contactForm">
+          <input type="text" placeholder="Your Name..." className="name" />
+          <input type="email" placeholder="Your Email..." className="email" />
+          <textarea
+            placeholder="Your Message..."
+            className="message"
+            rows="5"
+          ></textarea>
+          <button type="submit" value="Send" className="submitBtn">
+            Submit
+          </button>
+          <div className="links">
+            <img src={FacebookIcon} alt="" className="link" />
+            <img src={TwitterIcon} alt="" className="link" />
+            <img src={YoutubeIcon} alt="" className="link" />
+            <img src={InstagramIcon} alt="" className="link" />
+          </div>
+        </form>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
