200625088996497ecfffedeca0318f90c35c4e19
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-11-02 10:45:17 +0000

    Finished Footer

--- personal_website/src/components/Footer/footer.js
+++ personal_website/src/components/Footer/footer.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import './footer.css';
 
 const Footer = () => {
   return (
-    <footer className='footer'> 
+    <footer className="footer"> 
         Copyright &#169; 2023 Michael Wiciak. All right reserved.
     </footer>
-  )
+  );
 }
 
 export default Footer
