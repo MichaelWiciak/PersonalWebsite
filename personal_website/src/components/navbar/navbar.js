3cf3087eb1a6a40e5e4e18d510aef0006ba5a125
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-11-08 14:38:17 +0000

    N

--- personal_website/src/components/navbar/navbar.js
+++ personal_website/src/components/navbar/navbar.js
@@ -3,22 +3,33 @@ import './navbar.css'
 import logo from '../../assets/logo.png'
 import contact from '../../assets/contact.png'
 import { Link } from 'react-scroll'
+import menu from '../../assets/menu.png'
 
 export const Navbar = () => {
-  return (
-    <nav className="navbar"> 
-        <img src={logo} alt="logo" className="logo" />
-        <div className = "desktopMenu"> 
-            <Link className="desktopMenuButton">Home</Link>
-            <Link className="desktopMenuButton">About</Link>
-            <Link className="desktopMenuButton">Portfolio</Link>
-            <Link className="desktopMenuButton">Clients</Link>
-        </div>
-        <button className="desktopMenuBtn">
-            <img src={contact} alt="menu" className="desktopMenuImg" /> 
-            Contact Me    
-        </button>
-    </nav>
+    const [showMenu, setShowMenu] = useState(false);
+    return (
+        <nav className="navbar"> 
+            <img src={logo} alt="logo" className="logo" />
+            <div className = "desktopMenu"> 
+                <Link activeClass="active" to='intro' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuButton">Home</Link>
+                <Link activeClass="active" to='skills' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuButton">About</Link>
+                <Link activeClass="active" to='works' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuButton">Portfolio</Link>
+            </div>
+            <button className="desktopMenuBtn" onClick={() => {
+                document.getElementById("contact").scrollIntoView({behavior: "smooth"});
+            }}>
+                <img src={contact} alt="menu" className="desktopMenuImg" /> 
+                Contact Me    
+            </button>
+
+            <img src={menu} alt="logo" className="mobMenu" />
+            <div className = "navMenuMenu" style={{display: showMenu? 'flex:': 'none'}}> 
+                <Link activeClass="active" to='intro' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(!showMenu)}>Home</Link>
+                <Link activeClass="active" to='skills' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(!showMenu)}>About</Link>
+                <Link activeClass="active" to='works' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(!showMenu)}>Portfolio</Link>
+                <Link activeClass="active" to='contact' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(!showMenu)}>Contact</Link>
+            </div>
+        </nav>
    )
 }
 
