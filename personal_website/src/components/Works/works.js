229c579a24f93530d224e73a005a744cbbfcdd1e
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2023-11-10 20:30:30 +0000

    Finished Website

--- personal_website/src/components/Works/works.js
+++ personal_website/src/components/Works/works.js
@@ -1,27 +1,48 @@
 import React from 'react'
 import './works.css'
-import Portfolio1 from '../../assets/portfolio-1.png'
-import Portfolio2 from '../../assets/portfolio-2.png'
-import Portfolio3 from '../../assets/portfolio-3.png'
-import Portfolio4 from '../../assets/portfolio-4.png'
-import Portfolio5 from '../../assets/portfolio-5.png'
-import Portfolio6 from '../../assets/portfolio-6.png'
+import timetablex from '../../assets/timetablex.png'
+import stanford from '../../assets/stanford.png'
+import jackcompiler from '../../assets/jackcompiler.png'
+import pgm from '../../assets/pgm.png'
+import sportcentre from '../../assets/sportcentre.png'
+import videoPlayer from '../../assets/videoPlayer.png'
 
 
 const Works = () => {
+  const redirectStanford = () => {
+    window.location.href = "https://github.com/MichaelWiciak/Machine_Learning_by_Stanford_University_Programming_Exercises_Solutions";
+  };
+  const redirectTimetableX = () => {
+    window.location.href = "https://timetablex.com";
+  };
+  const redirectSports = () => {
+    window.location.href = "https://sports.feli.page/ ";
+  };
+  const redirectpgm = () => {
+    window.location.href = "https://github.com/MichaelWiciak/PGMFileManipulatorUtility";
+  };
+  const redirectJack= () => {
+    window.location.href = "https://github.com/MichaelWiciak/JACK_Compiler";
+  };
+  const redirectVideo = () => {
+    window.location.href = "https://github.com/MichaelWiciak/VideoPlayer";
+  };
+  const redirectGitHub = () => {
+    window.location.href = "https://github.com/MichaelWiciak?tab=repositories";
+  };
   return (
     <section id="works">
         <h2 className="worksTitle">My Portfolio</h2>
-        <span className="worksDesc">Loren I take pride in. Loren I take pride in. Loren I take pride in. Loren I take pride in. Loren I take pride in. Loren I take pride in. Loren I take pride in. Loren I take pride in.Loren I take pride in. Loren I take pride in. Loren I take pride in. Loren I take pride in.Loren I take pride in. Loren I take pride in. Loren I take pride in. Loren I take pride in.Loren I take pride in. Loren I take pride in. Loren I take pride in. Loren I take pride in. </span>
+        <span className="worksDesc">These are screenshots of some of the projects I worked on. They will take you to an active deployment or if not available, to the GitHub repo where the code is available. </span>
         <div className="worksImages">
-            <img src={Portfolio1} alt="" className="worksImg" />
-            <img src={Portfolio2}  alt="" className="worksImg" />
-            <img src={Portfolio3}  alt="" className="worksImg" />
-            <img src={Portfolio4}  alt="" className="worksImg" />
-            <img src={Portfolio5}  alt="" className="worksImg" />
-            <img src={Portfolio6}  alt="" className="worksImg" />
+            <img src={timetablex} alt="" className="worksImg" onClick={redirectTimetableX}/>
+            <img src={stanford}  alt="" className="worksImg" onClick={redirectStanford}/>
+            <img src={sportcentre}  alt="" className="worksImg" onClick={redirectSports}/>
+            <img src={videoPlayer}  alt="" className="worksImg" onClick={redirectVideo}/>
+            <img src={pgm}  alt="" className="worksImg" onClick={redirectpgm}/>
+            <img src={jackcompiler}  alt="" className="worksImg"  onClick={redirectJack}/>
         </div>
-        <button className="worksBtn">See More</button>
+        <button className="worksBtn" onClick={redirectGitHub}>See More</button>
     </section>
   )
 }
