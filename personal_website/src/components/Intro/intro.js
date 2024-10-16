264392c176eb84e01b5012e7f75a9cc1888249b3
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2024-10-16 19:19:12 +0100

    changed files

--- personal_website/src/components/Intro/intro.js
+++ personal_website/src/components/Intro/intro.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./intro.css";
 import bg from "../../assets/MW.JPG";
 import btnImg from "../../assets/hireme.png";
 import { Link } from "react-scroll";
 
+const titles = [
+  "Software Engineer",
+  "Amateur Photographer",
+  "Competitive Badminton Player",
+  "Kind-of Artist",
+  "Teacher",
+  "Student",
+  "IT Support",
+  "Web Developer",
+  "Hardware Engineer",
+  "Business Owner",
+  "Project Manager",
+  "Team Leader",
+];
+
 const Intro = () => {
+  const [currentTitle, setCurrentTitle] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTitle((prevTitle) => (prevTitle + 1) % titles.length);
+    }, 3000); // Switch every 3 seconds
+
+    return () => clearInterval(interval); // Clean up interval on component unmount
+  }, []);
+
   const redirectToWebsite = () => {
     window.location.href = "https://www.linkedin.com/in/michael-wiciak1/";
   };
+
   return (
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello,</span>
         <span className="introText">
-          I'm <span className="introName">Michael Wiciak</span> <br /> Software
-          Engineer
+          I'm <span className="introName">Michael Wiciak</span> <br />
+          <span className="animatedTitle">{titles[currentTitle]}</span>
         </span>
         <p className="introPara">
           I'm dedicated to excelling as a versatile software engineer,
