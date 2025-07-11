71f66408f0f3ec2672ce70f8528fba3e10e1354e
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2025-07-11 16:03:44 +0800

    update: bio, packages

--- personal_website/src/components/Skills/skills.js
+++ personal_website/src/components/Skills/skills.js
@@ -11,9 +11,7 @@ const Skills = () => {
         something cool for a group project.
         <br />
         <br />
-        My current playground is software engineering, and I’m about to graduate
-        with a degree in Computer Science with Artificial Intelligence from the
-        University of Leeds.
+        Right now, my playground is software engineering.
         <br />
         <br />
         I’ve gotten hands-on with everything from developing an app to automate
@@ -21,23 +19,13 @@ const Skills = () => {
         help students stay on top of deadlines.
         <br />
         <br />
-        Oh, and I co-founded TimetableX, a project that around a hundred
-        students keep their coursework organized while I flexed my React and
-        TypeScript skills.
+        I’m always chasing something new, whether it’s a coding challenge, a
+        cool project idea, or simply learning how to work smarter.
         <br />
         <br />
-        When I’m not deep into code, you’ll find me teaching others how to
-        solder RC cars, speaking Polish or basic Chinese for fun, or playing
-        badminton (I've even built tools to book the courts automatically—yes,
-        I’m that committed).
+        If you’re looking for someone who’s not afraid to roll up their sleeves
+        and get into the nitty-gritty of tech, I’m your person.
         <br />
-        <br />
-        I’ve got a decent handle on Python, C, C++, and a few other languages,
-        and if you challenge me with a new one, I’m game.
-        <br />
-        <br />
-        Overall, I’m always chasing something new, whether it’s a coding
-        challenge, a cool project idea, or simply learning how to work smarter.
       </span>
     </section>
   );
